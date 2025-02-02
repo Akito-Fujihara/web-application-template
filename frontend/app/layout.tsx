6a50264366f043c1f37e4874bf3180cd3ee914eb
Akito-Fujihara <1998922akito@gmail.com>
2025-02-02 17:16:01 +0900

    nextjs app の作成

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,13 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="jp">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Web Application Template</h1>
+    </div>
+  );
+}
