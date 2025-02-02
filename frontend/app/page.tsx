ace9bd6a38434e2b1872acc1fb11823ddfdc14b4
Akito-Fujihara <1998922akito@gmail.com>
2025-02-02 18:17:26 +0900

    TailwindCSS の導入

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Web Application Template</h1>
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold underline">Web Application Template</h1>
     </div>
   );
 }
